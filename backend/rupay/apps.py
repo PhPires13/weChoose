7d04688f633ca4d0ec6d4ba4d1e1f9300eb94dca
Pedro Pires <pires.phgp@gmail.com>
2026-04-11 16:13:12 -0300

    Reorganize project folders

--- backend/rupay/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RupayConfig(AppConfig):
-    name = 'rupay'
---
+++ rupayapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RupayappConfig(AppConfig):
+    name = 'rupayapp'
